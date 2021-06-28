811c9e5eb6b4a3666fb2145a4a0b64f5677d9c9d
CurtisNewbie <zhuangyongj@gmail.com>
2021-06-28 23:42:07 +0800

    Fix doc

--- ioc-module/src/main/java/com/curtisnewbie/module/ioc/annotations/MBean.java
+++ ioc-module/src/main/java/com/curtisnewbie/module/ioc/annotations/MBean.java
@@ -3,8 +3,8 @@ package com.curtisnewbie.module.ioc.annotations;
 import java.lang.annotation.*;
 
 /**
- * Indicating that this class should be managed as a bean, which is managed as a bean by the container, it might also be
- * injected into another bean as a dependency
+ * Indicating that this class should be managed as a bean by the container, it might also be injected into another bean
+ * as a dependency
  *
  * @author yongjie.zhuang
  */
